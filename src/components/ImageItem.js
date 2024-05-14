d818d0fa5b0384599943eebb674117b7d84189cc
yifan503 <yifan503@outlook.com>
2024-05-15 00:55:49 +0800

    image item style

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import styles from './imageItem.module.css';
 
 export default function ImageItem({ src, alt, text, buttonText, page = '/' }) {
 
   return (
-    <div className="item">
-      <img src={src} alt={alt} />
-      <div className="overlay-item">
-        <div className="image-text">{text}</div>
+    <div className={styles.item}>
+      <img src={src} alt={alt} className={styles.img} />
+      <div className={styles.overlay_item}>
+        <div className={styles.image_text}>{text}</div>
         <Link href={page}>
-          <button className="image-button">{buttonText}</button>
+          <button className={styles.image_button}>{buttonText}</button>
         </Link>
       </div>
     </div>
