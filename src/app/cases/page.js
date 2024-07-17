60e1e7addb98e07f39a81f03288d8d3c4c5e5802
yifan503 <yifan503@outlook.com>
2024-07-18 01:08:36 +0800

    case page optimize

--- src/app/cases/page.js
+++ src/app/cases/page.js
@@ -6,13 +6,10 @@ export default function CasesPage() {
         <>
             <div className={styles.title}>精选现场案例</div>
             <div className={styles.galleryContainer}>
-                <TypeImage src='/image/qxhy_gate.JPG' title='清溪花园别墅299号' id='1' />
-                <TypeImage src='/image/cupboard.png' title='西郊庄园别墅' />
-                <TypeImage src='/image/cupboard_type.png' title='金虹别墅' />
-            </div>
-            <div className={styles.galleryContainer2}>
-                <TypeImage src='/image/cupboard_type.png' title='御翠园别墅' />
-                <TypeImage src='/image/cupboard.png' title='玫瑰园别墅' />
+                <TypeImage src='/image/qxhy_gate.JPG' title='清溪花园别墅299号' year='2012' project='全屋定制、橱柜、木饰面' />
+                <TypeImage src='/image/cupboard.png' title='玫瑰园别墅' year='2013' project='全屋定制' />
+                <TypeImage src='/image/cupboard.png' title='西郊庄园别墅' year='2015' project='衣帽间、橱柜、书柜、双开门' />
+                <TypeImage src='/image/cupboard_type.png' title='金虹别墅' year='2017' project='酒柜、书柜、木饰面' />
             </div>
 
         </>
--- src/app/cases/typesImage.js
+++ src/app/cases/typesImage.js
@@ -7,7 +7,9 @@ export default function TypeImage(props) {
     return (
         <div className={styles.img_container}>
             <img src={props.src} alt="table image" className={styles.image} />
-            <div className={styles.image_title}>{props.title}</div>
+            <div className={styles.image_title}><span>{props.title}</span></div>
+            <div className={styles.image_info}>{props.year}年作品</div>
+            <div className={styles.image_info}>{props.project}</div>
         </div>
     )
 }
