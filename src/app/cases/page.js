d45da37688afac5ba5e431224da5d5d36984c483
yifan503 <yifan503@outlook.com>
2024-07-18 17:05:28 +0800

    types page

--- src/app/cases/page.js
+++ src/app/cases/page.js
@@ -1,17 +1,33 @@
+'use client'
+
+import { useState } from 'react'
 import styles from './cases.module.css'
 import TypeImage from './typesImage'
 
 export default function CasesPage() {
+
+    const [showDetail, setShow] = useState(false)
+
+    const handleclick = () => {
+        setShow(true)
+    }
+
+    const closeDetail = () => {
+        setShow(false)
+    }
+
     return (
         <>
             <div className={styles.title}>精选现场案例</div>
             <div className={styles.galleryContainer}>
-                <TypeImage src='/image/qxhy_gate.JPG' title='清溪花园别墅299号' year='2012' project='全屋定制、橱柜、木饰面' />
-                <TypeImage src='/image/cupboard.png' title='玫瑰园别墅' year='2013' project='全屋定制' />
-                <TypeImage src='/image/cupboard.png' title='西郊庄园别墅' year='2015' project='衣帽间、橱柜、书柜、双开门' />
-                <TypeImage src='/image/cupboard_type.png' title='金虹别墅' year='2017' project='酒柜、书柜、木饰面' />
+                <TypeImage cb={handleclick} src='/image/qxhy_gate.JPG' title='清溪花园别墅299号' year='2012' project='全屋定制、橱柜、木饰面' />
+                <TypeImage cb={handleclick} src='/image/cupboard.png' title='玫瑰园别墅' year='2013' project='全屋定制' />
+                <TypeImage cb={handleclick} src='/image/cupboard.png' title='西郊庄园别墅' year='2015' project='衣帽间、橱柜、书柜、双开门' />
+                <TypeImage cb={handleclick} src='/image/cupboard_type.png' title='金虹别墅' year='2017' project='酒柜、书柜、木饰面' />
             </div>
-
+            {showDetail && <div className={styles.bg} onClick={closeDetail}>
+                <img className={styles.detail_img} src='/image/qxhy_gate.JPG' alt='img' />
+            </div>}
         </>
     )
 }
--- src/app/cases/typesImage.js
+++ src/app/cases/typesImage.js
@@ -1,15 +1,18 @@
 'use client'
 
+import { useState } from 'react'
 import styles from './cases.module.css'
 
 export default function TypeImage(props) {
 
     return (
-        <div className={styles.img_container}>
-            <img src={props.src} alt="table image" className={styles.image} />
-            <div className={styles.image_title}><span>{props.title}</span></div>
-            <div className={styles.image_info}>{props.year}年作品</div>
-            <div className={styles.image_info}>{props.project}</div>
-        </div>
+        <>
+            <div className={styles.img_container}>
+                <img src={props.src} alt="table image" className={styles.image} onClick={props.cb} />
+                <div className={styles.image_title}><span>{props.title}</span></div>
+                <div className={styles.image_info}>{props.year}年作品</div>
+                <div className={styles.image_info}>{props.project}</div>
+            </div>
+        </>
     )
 }
