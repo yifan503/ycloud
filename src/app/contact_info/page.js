605ab669cf7cf772fe59169fb3546fec3f400f04
yifan503 <yifan503@outlook.com>
2024-07-20 19:25:21 +0800

    home page fullpage.js

--- src/app/contact_info/page.js
+++ src/app/contact_info/page.js
@@ -1,7 +1,37 @@
+'use client'
+
+import ReactFullpage from '@fullpage/react-fullpage';
+
 export default function ContactInfo() {
+
+    const Fullpage = () => (
+        <ReactFullpage
+            //fullpage options
+            licenseKey={'gplv3-license'}
+            scrollingSpeed={700} /* Options here */
+            credits={{ enabled: true, label: '', position: 'right' }}
+            render={({ state, fullpageApi }) => {
+                return (
+                    <ReactFullpage.Wrapper>
+                        <div className="section">
+                            <p>Section 1 (welcome to fullpage.js)</p>
+
+                        </div>
+                        <div className="section">
+                            <p>Section 2</p>
+                        </div>
+                        <div className="section">
+                            <p>Section 3</p>
+                        </div>
+                    </ReactFullpage.Wrapper>
+                );
+            }}
+        />
+    );
+
     return (
         <>
-            phone number
+            <Fullpage />
         </>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,61 @@
+'use client'
+
 import ImageItem from '../components/ImageItem'
+import ReactFullpage from '@fullpage/react-fullpage';
 
 export default function Home() {
+
+  const Fullpage = () => (
+    <ReactFullpage
+      //fullpage options
+      licenseKey={'gplv3-license'}
+      scrollingSpeed={700} /* Options here */
+      credits={{ enabled: true, label: '', position: 'right' }}
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <ImageItem
+                src="/image/closet.png"
+                alt="cloakroom"
+                text="步入式衣帽间方案"
+                buttonText="即刻探索"
+                page='/closet'
+              />
+            </div>
+            <div className="section">
+              <ImageItem
+                src="/image/cupboard2.png"
+                alt="cupboard"
+                text="橱柜定制案例"
+                buttonText="查看更多"
+              />
+            </div>
+            <div className="section">
+              <ImageItem
+                src="/image/cabinet.png"
+                alt="cabinet"
+                text="设计师装饰柜系列"
+                buttonText="即刻探索"
+              />
+            </div>
+            <div className="section">
+              <ImageItem
+                src="/image/more_case.png"
+                alt="moreProduct"
+                text="更多案例鉴赏"
+                buttonText="点击查看"
+              />
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
+  );
   return (
     <>
-      <div className="container">
-        <ImageItem
-          src="/image/closet.png"
-          alt="cloakroom"
-          text="步入式衣帽间方案"
-          buttonText="即刻探索"
-          page='/closet'
-        />
-        <ImageItem
-          src="/image/cupboard2.png"
-          alt="cupboard"
-          text="橱柜定制案例"
-          buttonText="查看更多"
-        />
-        <ImageItem
-          src="/image/cabinet.png"
-          alt="cabinet"
-          text="设计师装饰柜系列"
-          buttonText="即刻探索"
-        />
-        <ImageItem
-          src="/image/more_case.png"
-          alt="moreProduct"
-          text="更多案例鉴赏"
-          buttonText="点击查看"
-        />
-      </div>
+
+      <Fullpage />
     </>
   )
 }
