91de73e2cc9b799fbf8c5e9d95d1d3ad72aa2a51
yifan503 <yifan503@outlook.com>
2024-08-07 17:57:07 +0800

    closet page

--- src/app/closet/page.js
+++ src/app/closet/page.js
@@ -6,7 +6,24 @@ export default function Closet() {
         <>
             <div className={styles.container}>
                 <div className={styles.row}>
-                    <img src='/image/wardrobes.png' alt='wardrobes' className={styles.img} />
+                    <img src='/image/wardrobes.png' alt='wardrobes' className={styles.head_img} />
+                    <div className={styles.text}>
+                        <h1>衣帽间定制</h1>
+                        <p>衣帽间的精致除了科学合理的收纳分区布局外，</p>
+                        <span>结构、美感与灯光也是极为重要的成分。</span>
+                        <p>高定衣帽间从居住者的生活中汲取灵感，</p>
+                        <p>匹配居住者的高级审美和品味生活需求。</p>
+                    </div>
+                </div>
+                <div className={styles.intro_text_container}>
+                    <h2>衣帽间案例精选</h2>
+                    <div className={styles.intro_text}>造型简单却设计感十足，金属框架打造的产品也不会呆板，让居住的生活简单而又富有精致美感。</div>
+                </div>
+                <div className={styles.row}>
+                    <img src='/image/wardrobes2.png' alt='wardrobes' className={styles.img} />
+                    <img src='/image/wardrobes3.jpg' alt='wardrobes' className={styles.img} />
+                    <img src='/image/wardrobes4.jpg' alt='wardrobes' className={styles.img} />
+
 
                 </div>
                 {/* <div className={styles.column}>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div>footer</div>
-            <div className={styles.bottom}>Copyright © 2023 - 2024 Y NUVOLA. All Rights Reserved. 上海亿云建材 版权所有 </div>
+            <div className={styles.bottom}>Copyright © 2023 - 2024 YIYUN. All Rights Reserved. 上海亿云建材 版权所有 </div>
         </footer>
     )
 }
