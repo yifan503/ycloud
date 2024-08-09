6cb553c3662a79b24b9afd5ad28a334e4cddca17
yifan503 <yifan503@outlook.com>
2024-08-09 19:53:58 +0800

    footer

--- src/app/contact_info/page.js
+++ src/app/contact_info/page.js
@@ -1,37 +1,18 @@
 'use client'
 
-import ReactFullpage from '@fullpage/react-fullpage';
+import styles from './contact.module.css';
 
 export default function ContactInfo() {
 
-    const Fullpage = () => (
-        <ReactFullpage
-            //fullpage options
-            licenseKey={'gplv3-license'}
-            scrollingSpeed={700} /* Options here */
-            credits={{ enabled: true, label: '', position: 'right' }}
-            render={({ state, fullpageApi }) => {
-                return (
-                    <ReactFullpage.Wrapper>
-                        <div className="section">
-                            <p>Section 1 (welcome to fullpage.js)</p>
-
-                        </div>
-                        <div className="section">
-                            <p>Section 2</p>
-                        </div>
-                        <div className="section">
-                            <p>Section 3</p>
-                        </div>
-                    </ReactFullpage.Wrapper>
-                );
-            }}
-        />
-    );
-
     return (
         <>
-            <Fullpage />
+            <div className={styles.container}>
+                <div>定制流程</div>
+                <div>联系我们</div>
+                <div>地址：</div>
+                <div>电话：</div>
+                <div>微信：</div>
+            </div>
         </>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,25 @@ import styles from './footer.module.css';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div>footer</div>
+            {/* <div className={styles.name}>
+                <strong>亿云</strong>
+            </div> */}
+            <div className={styles.row}>
+                <div className={styles.title}>
+                    <div className={styles.text}>始于2003年</div>
+                    <div className={styles.text}>别墅｜豪宅｜高端定制整体解决方案</div>
+                    <div className={styles.text}>全品类配套一站式私人订制服务</div>
+                    <div className={styles.text}>联系电话：13901839676 梁先生</div>
+                </div>
+                <div className={styles.types}>
+                    <span className={styles.text}>衣柜 / 橱柜 / 书柜 / 酒柜</span>
+                    <span className={styles.text}>房门 / 移门 / 折叠门</span>
+                    <span className={styles.text}>护墙板 / 楼梯</span>
+                    <span className={styles.text}>意大利进口家居</span>
+                </div>
+                <div className={styles.qrcode}>二维码</div>
+
+            </div>
             <div className={styles.bottom}>Copyright © 2023 - 2024 YIYUN. All Rights Reserved. 上海亿云建材 版权所有 </div>
         </footer>
     )
