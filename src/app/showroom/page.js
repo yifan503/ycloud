f24fca8c926f491dd532c9c31f29f41f39233018
yifan503 <yifan503@outlook.com>
2024-08-29 19:36:13 +0800

    arrow css

--- src/app/showroom/page.js
+++ src/app/showroom/page.js
@@ -4,6 +4,7 @@ import styles from './showroom.module.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import './slick.css';
 
 export default function ShowRoom() {
 
